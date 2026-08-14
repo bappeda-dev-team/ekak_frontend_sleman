7dc884480eac1b05e1f5ee98e8b5290346aca7f1
mykoakbar <yeikoakbar20@gmail.com>
2026-08-14 16:16:53 +0700

    add captcha, add timer login dummy, add alert change password (maintenance)

--- src/app/(main)/login/page.tsx
+++ src/app/(main)/login/page.tsx
@@ -3,16 +3,22 @@
 import Image from "next/image";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { TbEye, TbEyeClosed } from "react-icons/tb";
+import { useEffect, useState } from "react";
+import { TbEye, TbEyeClosed, TbRefresh } from "react-icons/tb";
 import { ButtonSky } from "@/components/global/Button";
 import { LoadingButtonClip } from "@/components/global/Loading";
 import { login } from "@/components/lib/Cookie";
 import { useBrandingContext } from "@/context/BrandingContext";
 
 interface FormValues {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
+    captcha_key: string;
+    captcha_answer: string;
+}
+interface CaptchaData {
+    captcha_id: string;
+    captcha_image: string;
 }
 
 const LoginPage = () => {
@@ -20,101 +26,178 @@ const LoginPage = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [Proses, setProses] = useState<boolean>(false);
-    const router = useRouter();
 
+    const [Captcha, setCaptcha] = useState<CaptchaData | null>(null);
+    const [Loading, setLoading] = useState<boolean>(false);
+    const [FetchCaptcha, setFetchCaptcha] = useState<boolean>(false);
+    const [ErrorCaptcha, setErrorCaptcha] = useState<boolean>(false);
+
+    const [TimeLeft, setTimeLeft] = useState<number>(300); // 5 menit
+    const [TimeExpired, setTimeExpired] = useState<boolean>(false);
+
+    const router = useRouter();
     const { branding } = useBrandingContext();
 
     const onSubmit: SubmitHandler<FormValues> = async (data) => {
-        //   console.log(data.username, data.password);
         setProses(true);
-        try{
-            const isLoggedIn = await login(data.username, data.password);
+        try {
+            const isLoggedIn = await login(data.username, data.password, Captcha?.captcha_id ?? "", data.captcha_answer);
             if (isLoggedIn) {
-              router.push('/'); // Redirect ke halaman dashboard jika login berhasil
+                router.push('/'); // Redirect ke halaman dashboard jika login berhasil
             }
-        } catch(error) {
+        } catch (error) {
             console.error(error)
         } finally {
             setProses(false);
         }
     };
 
-    return(
+    useEffect(() => {
+        const API_URL = process.env.NEXT_PUBLIC_API_URL;
+        const fetchCaptcha = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch(`${API_URL}/user/captcha`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                const result = await response.json();
+                if (result.code === 200) {
+                    setCaptcha(result.data);
+                    setErrorCaptcha(false);
+                    // console.log(result);
+                } else {
+                    setCaptcha(null);
+                    setErrorCaptcha(true);
+                }
+            } catch (err) {
+                console.error(err)
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchCaptcha();
+    }, [FetchCaptcha]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft((prev) => {
+                if (prev <= 1) {
+                    clearInterval(timer);
+                    return 0;
+                }
+
+                return prev - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    const minutes = Math.floor(TimeLeft / 60);
+    const seconds = TimeLeft % 60;
+
+    const TimerText = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+    return (
         <>
-        <div className="flex items-center justify-center w-full h-screen bg-gray-100">
-            <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-8 rounded-lg shadow-md w-96">
-                <div className="flex flex-col items-center">
-                    <Image 
-                        src={branding.logo}
-                        // src="/universal.png"
-                        alt="logo" 
-                        width={90}
-                        height={90} 
-                    />
-                    <h1 className="text-2xl font-bold mt-3 text-center uppercase">{branding.title}</h1>
-                    <h1 className="text-lg font-thin mb-6 text-center">{branding.client}</h1>
-                </div>
-                {/* NIP */}
-                <div className="mb-4">
-                    <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-1">
-                        NIP
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
-                        {...register('username', { required: 'nip harus terisi' })}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
-                    />
-                    {errors.username && <span className="text-red-500 text-sm">{errors.username.message}</span>}
-                </div>
-                {/* PW */}
-                <div className="mb-6">
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-                        Password
-                    </label>
-                    <div className="flex items-center justify-end">
-                        <input
-                            type={!showPassword ? 'password' : 'text'}
-                            id="password"
-                            {...register('password', { required: 'password harus terisi' })}
-                            className="mt-1 block w-full pl-3 pr-10 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+            <div className="flex items-center justify-center w-full h-screen bg-gray-100">
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-8 rounded-lg shadow-md w-96">
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={branding.logo}
+                            // src="/universal.png"
+                            alt="logo"
+                            width={90}
+                            height={90}
                         />
-                        <button
-                            type="button"
-                            className="absolute mt-1 mr-3 text-sm"
-                            onClick={() => setShowPassword(!showPassword)}
-                            >
-                            {showPassword ? <TbEye /> : <TbEyeClosed />}
-                        </button>
+                        <h1 className="text-2xl font-bold mt-3 text-center uppercase">{branding.title}</h1>
+                        <h1 className="text-lg font-thin mb-6 text-center">{branding.client}</h1>
                     </div>
-                    {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
-                </div>
-                {/* <select
-                    className="w-full"
-                    value={SelectedUser}
-                    onChange={(e) => setSelectedUser(e.target.value)}
-                >
-                    <option value="">pilih role</option>
-                    <option value="super_admin">super admin</option>
-                    <option value="admin_opd">admin opd</option>
-                    <option value="asn">asn</option>
-                </select> */}
-                <ButtonSky
-                    type="submit"
-                    className="w-full"
-                    disabled={Proses}
-                >
-                    {Proses ? 
-                        <span className="flex">
-                            <LoadingButtonClip />
-                            Login...
-                        </span> 
-                    :
-                        "Login"
+                    {/* NIP */}
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700 mb-1">
+                            NIP
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            {...register('username', { required: 'nip harus terisi' })}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                        />
+                        {errors.username && <span className="text-red-500 text-sm">{errors.username.message}</span>}
+                    </div>
+                    {/* PW */}
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+                            Password
+                        </label>
+                        <div className="flex items-center justify-end">
+                            <input
+                                type={!showPassword ? 'password' : 'text'}
+                                id="password"
+                                {...register('password', { required: 'password harus terisi' })}
+                                className="mt-1 block w-full pl-3 pr-10 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                            />
+                            <button
+                                type="button"
+                                className="absolute mt-1 mr-3 text-sm"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <TbEye /> : <TbEyeClosed />}
+                            </button>
+                        </div>
+                        {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
+                    </div>
+                    {/* CAPTCHA */}
+                    {Loading ?
+                        <p>Loading captcha...</p>
+                        :
+                        ErrorCaptcha ?
+                            <h1 className="text-red-500">error saat mendapatkan gambar capthca, cek koneksi internet atau server</h1>
+                            :
+                            <div className="flex flex-col items-center gap-1">
+                                <Image src={Captcha?.captcha_image ?? ""} width={180} height={70} alt="Captcha" />
+                                <div className="mb-4">
+                                    <div className="flex items-center gap-1">
+                                        <input
+                                            type="text"
+                                            id="captcha_answer"
+                                            placeholder="masukkan kode diatas"
+                                            {...register('captcha_answer', { required: 'Captcha harus terisi' })}
+                                            className="block w-full px-3 py-2 border text-center border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                                        />
+                                        <button
+                                            type="button"
+                                            className="text-blue-500 italic text-sm font-light border p-3 rounded-lg hover:bg-blue-200 hover:text-white"
+                                            onClick={() => setFetchCaptcha((prev) => !prev)}
+                                        >
+                                            <TbRefresh />
+                                        </button>
+                                    </div>
+                                    {errors.captcha_answer && <span className="text-red-500 text-sm">{errors.captcha_answer.message}</span>}
+                                </div>
+                            </div>
                     }
-                </ButtonSky>
-            </form>
-        </div>
+                    <ButtonSky
+                        type="submit"
+                        className="w-full"
+                        disabled={Proses || TimeLeft > 0}
+                    >
+                        {Proses ? (
+                            <span className="flex">
+                                <LoadingButtonClip />
+                                Login...
+                            </span>
+                        ) : TimeLeft > 0 ? (
+                            `Tunggu ${TimerText}`
+                        ) : (
+                            "Login"
+                        )}
+                    </ButtonSky>
+                </form>
+            </div>
         </>
     )
 }
