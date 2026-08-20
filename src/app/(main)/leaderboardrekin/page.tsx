736919fadaa667a66904733f55a956bca40913b9
mykoakbar <yeikoakbar20@gmail.com>
2026-08-20 12:13:38 +0700

    fix error

--- src/app/(main)/leaderboardrekin/page.tsx
+++ src/app/(main)/leaderboardrekin/page.tsx
@@ -1,20 +1,44 @@
 'use client'
 
+import { useState, useEffect } from "react";
+import { ButtonBlackBorder } from "@/components/global/Button";
 import { FiHome } from "react-icons/fi";
 import { Table } from "./comp/Table";
 import { useBrandingContext } from "@/context/BrandingContext";
+import { IsLoadingBranding } from "@/components/global/Loading";
 import { TahunNull } from "@/components/global/OpdTahunNull";
+import { TbRefresh } from "react-icons/tb";
+import { getUser } from "@/components/lib/Cookie";
 
 const ControlPokin = () => {
 
-    const { branding } = useBrandingContext();
+    const { branding, LoadingBranding } = useBrandingContext();
+    const [User, setUser] = useState<any>(null);
+
+    useEffect(() => {
+        const get_user = getUser();
+        if(get_user){
+            setUser(get_user.user);
+        }
+    }, [])
 
     if (branding?.tahun?.value === undefined || branding?.tahun?.value === null) {
         return (
             <TahunNull />
         )
-    }
-    else {
+    } else if(LoadingBranding) {
+        return <IsLoadingBranding />
+    } else if(branding?.user === null) {
+        return(
+            <div className="flex items-center gap-1">
+                <h1>Reload Halaman</h1>
+                <ButtonBlackBorder>
+                    <TbRefresh />
+                    Reload
+                </ButtonBlackBorder>
+            </div>
+        )
+    } else {
         return (
             <>
                 <div className="flex items-center">
@@ -27,7 +51,7 @@ const ControlPokin = () => {
                         <h1 className="font-bold text-lg uppercase">Leaderboard Rekin {branding?.tahun?.label}</h1>
                     </div>
                     <div className="flex m-2">
-                        <Table tahun={branding?.tahun?.value}/>
+                        <Table tahun={branding?.tahun?.value} user={User}/>
                     </div>
                 </div>
             </>
